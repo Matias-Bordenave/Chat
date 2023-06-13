264cc990b022078c90857bb7378a8888168317c1
Matias-Bordenave <116688361+Matias-Bordenave@users.noreply.github.com>
2023-06-13 00:02:10 -0300

    Add files via upload

---
+++ Chat/public/js/index.js
@@ -0,0 +1,10 @@
+
+const socket = io()
+
+socket.on('hola',(data)=>{
+console.log(data)
+socket.emit('holavuelta', 'Gracias por la conexion')
+socket.on('holavuelta', (data)=>{
+    console.log(data)
+})
+})
